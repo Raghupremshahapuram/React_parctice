d1ab0a33ad3d274a48d89677225b4f9da0fe2da3
Raghupremshahapuram <100329614+Raghupremshahapuram@users.noreply.github.com>
2025-01-21 14:54:15 +0530

    Added Navigation

---
+++ src/Navigation/Customer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Customer extends React.Component{
+    render(){
+        return(
+            <h1>customer</h1>
+        );
+    }
+
+};
+export default Customer;
---
+++ src/Navigation/Prodcuts1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Products1 =()=>{
+return(
+    <h1>Product </h1>
+)
+
+};
+export default Products1;
